8ad13559c01c66b54794c3a4d596abc4d4c89bef
fragalado <frandanigd@gmail.com>
2023-02-22 10:15:10 +0100

    Calculo DNI

--- edu.java.calculaDNI/src/aplicacion/controladores/Principal.java
+++ edu.java.calculaDNI/src/aplicacion/controladores/Principal.java
@@ -1,5 +1,7 @@
 package aplicacion.controladores;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 import aplicacion.entidades.Dni;
@@ -12,7 +14,9 @@ import aplicacion.entidades.Dni;
 public class Principal {
 
 	public static void main(String[] args) {
-		// 
+		// lista
+		List<Dni> listaBD = new ArrayList<>();
+		
 		// Inicializamos el objeto de tipo Dni
 		Dni res = new Dni();
 		
@@ -24,53 +28,62 @@ public class Principal {
 		// Scanner para leer los datos
 		Scanner sc = new Scanner(System.in);
 		
-		// Numero dni
-		System.out.print("Introduzca el número del DNI: ");
-		numDNIUsuario = sc.nextInt();
-		
-		// Letra dni
-		System.out.print("Introduzca la letra del DNI: ");
-		letraDNIUsuario = sc.next().toUpperCase().charAt(0);
-		
-		// Ahora comprobamos que el numero del DNI está bien introducido, es decir...
-		// Es mayor que 0 y menor que 99999999
-		while(numDNIUsuario < 0 || numDNIUsuario > 99999999) {
-			// Mostramos mensaje de error al usuario
-			System.err.println("** Error: El número de DNI introducido no cumple los requisitos **");
-		
-			// Volvemos a pedir el número
-			System.out.print("\nIntroduzca el número del DNI: ");
-			numDNIUsuario = sc.nextInt();
-		}
-		
-		// Una vez que tenemos un numero de DNI válido
-		// Vamos a calcular la letra del numero pasado y después vamos a compararla con la letra
-		// introducida por el usuario
-		char letraDNI = res.calculaLetra(numDNIUsuario);
-		
-		// Ahora comparamos las letras
-		// Si no coinciden mostraremos mensaje de error y volveremos a pedir la letra
-		while(letraDNI != letraDNIUsuario) {
-			// Mensaje de error
-			System.err.println("\n** Error: La letra que ha indicado no es correcta");
+		int opcion;
+		char letraDNI;
+		do {
+			// Mostramos el Menu
+			Menu();
+			opcion = sc.nextInt();
 			
-			// Pedimos la letra
-			System.out.print("\nIntroduzca la letra del DNI: ");
-			letraDNIUsuario = sc.next().toUpperCase().charAt(0);
-		}
+			switch (opcion) {
+			case 1:
+				// Numero dni
+				System.out.print("Introduzca el número del DNI: ");
+				numDNIUsuario = sc.nextInt();
+				
+				// Letra dni
+				System.out.print("Introduzca la letra del DNI: ");
+				letraDNIUsuario = sc.next().toUpperCase().charAt(0);
+				
+				// Ahora comprobamos que el numero del DNI está bien introducido, es decir...
+				// Es mayor que 0 y menor que 99999999
+				numDNIUsuario = res.compruebaNumeroDNI(numDNIUsuario); // Si el numero está bien.
+																			// nos devolverá el mismo numero
+				
+				// Una vez que tenemos un numero de DNI válido
+				// Vamos a calcular la letra del numero pasado y después vamos a compararla con la letra
+				// introducida por el usuario
+				letraDNI = res.calculaLetra(numDNIUsuario);
+				
+				// Ahora comparamos las letras
+				// Si no coinciden mostraremos mensaje de error y volveremos a pedir la letra
+				letraDNIUsuario = res.compruebaLetraDNI(letraDNIUsuario, letraDNI);
+				
+				// Una vez que tenemos el numero y la letra correcta, mostraremos un mensaje indicando que es correcto
+				System.out.println("El número y la letra de DNI son correctos");
+				
+				// Añadimos a la lista el objeto
+				listaBD.add(res.creaObjetoDni(numDNIUsuario, letraDNI));
+				
+				break;
+				case 2:
+					res.mostrarListaDni(listaBD);
+					break;
+			}
+			
+			
+		} while (opcion != 0);
 		
-		// Una vez que tenemos el numero y la letra correcta, mostraremos un mensaje indicando que es correcto
-		System.out.println("El número y la letra de DNI son correctos");
+		
 		
 		// Cerramos Scanner
 		sc.close();
-		
-		// Añadimos el numero y la letra al objeto creado
-		res.setNumeroDNI(numDNIUsuario);
-		res.setLetraDNI(letraDNI);
-		
-		// Mostramos el objeto por pantalla
-		System.out.println(res.toString());
 	}
-
+	
+	
+	private static void Menu() {
+		System.out.println("1. Introduce DNI");
+		System.out.println("2. Muestra lista");
+		System.out.println("0. Salir");
+	}
 }
--- edu.java.calculaDNI/src/aplicacion/entidades/Dni.java
+++ edu.java.calculaDNI/src/aplicacion/entidades/Dni.java
@@ -1,5 +1,8 @@
 package aplicacion.entidades;
 
+import java.util.List;
+import java.util.Scanner;
+
 /**
  * 
  * @author csi22
@@ -44,6 +47,79 @@ public class Dni {
 		
 		// Vamos a devolver la letra, luego:
 		return letras[numDNI % 23];
+	}
+	
+	/**
+	 * Comprueba que el numero del DNI introducido sea correcto
+	 * @param numDNI
+	 * @return
+	 */
+	public int compruebaNumeroDNI(int numDNI) {
+		// Scanner para leer
+		Scanner entrada = new Scanner(System.in);
+		
+		// Comprobamos que el numero del DNI está bien introducido, es decir...
+		// Es mayor que 0 y menor que 99999999
+		while(numDNI < 0 || numDNI > 99999999) {
+			// Mostramos mensaje de error al usuario
+			System.err.println("** Error: El número de DNI introducido no cumple los requisitos **");
+		
+			// Volvemos a pedir el número
+			System.out.print("\nIntroduzca el número del DNI: ");
+			numDNI = entrada.nextInt();
+		}
+		return numDNI;
+	}
+	
+	/**
+	 * Comprueba que la letra del DNI introducido sea correcta
+	 * @param letraUsu
+	 * @param letraProgram
+	 * @return
+	 */
+	public char compruebaLetraDNI(char letraUsu, char letraProgram) {
+		// Scanner para leer
+		Scanner valores = new Scanner(System.in);
+		
+		// Si no coinciden mostraremos mensaje de error y volveremos a pedir la letra
+		while(letraProgram != letraUsu) {
+			// Mensaje de error
+			System.err.println("\n** Error: La letra que ha indicado no es correcta");
+			
+			// Pedimos la letra
+			System.out.print("\nIntroduzca la letra del DNI: ");
+			letraUsu = valores.next().toUpperCase().charAt(0);
+		}
+		return letraUsu;
+	}
+	
+	/**
+	 * Metodo para mostrar cada objeto de tipo dni de la base de datos
+	 * @param listaBD
+	 */
+	public void mostrarListaDni(List<Dni> listaBD) {
+		
+		for (Dni aux : listaBD) {
+			
+			System.out.println(aux.toString());
+		}
+	}
+	
+	/**
+	 * Crea un objeto de tipo DNI 
+	 * @param numDNI
+	 * @param letraDni
+	 * @return Devuelve el objeto creado de tipo DNI
+	 */
+	public Dni creaObjetoDni(int numDNI, char letraDni) {
+		
+		// Inicializamos el objeto de tipo Dni
+		Dni aux = new Dni();
+		
+		aux.setNumeroDNI(numDNI);
+		aux.setLetraDNI(letraDni);
+		
+		return aux;
 	}
 	
 	// toString
